3044a6469d1561decc8b206200f567f3756c6b5d
Caleb Falcione <caleb.falcione@gmail.com>
2018-01-27 15:36:18 -0600

    Extended setup script

--- terminal-setup.sh
+++ terminal-setup.sh
@@ -5,3 +5,9 @@ sudo apt install wget git zsh vim htop
 sh -c "$(wget https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
 
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+
+
+git config --global user.name "Caleb Falcione"
+git config --global user.email "caleb.falcione@gmail.com"
+git config --global push.default simple
+git config --global credential.helper store
