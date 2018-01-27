799262f5143343926e5fba921b1bac52e6c7468a
Caleb Falcione <caleb.falcione@gmail.com>
2018-01-27 15:34:44 -0600

    Removed oh-my-zsh folder and added setup script

---
+++ terminal-setup.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+sudo apt install wget git zsh vim htop
+
+sh -c "$(wget https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
+
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
