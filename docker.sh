9d2fcab33d7b9cda560a07808486060d44f1d068
Caleb <caleb.falcione@gmail.com>
2018-10-18 14:38:23 -0500

    updates docker-compose

--- docker.sh
+++ docker.sh
@@ -12,6 +12,6 @@ sudo usermod -aG docker $USER
 
 echo Added $USER to docker group. Installing docker-compose.
 
-COMPOSE_RELEASE=1.21.2
+COMPOSE_RELEASE=1.22.0
 sudo curl -L https://github.com/docker/compose/releases/download/$COMPOSE_RELEASE/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose > /dev/null
 sudo chmod +x /usr/local/bin/docker-compose
