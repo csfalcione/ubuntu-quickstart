c883be90fbd2350519d89368081a890909412684
Caleb <caleb.falcione@gmail.com>
2018-10-18 10:08:59 -0500

    adds scripts for common languages

---
+++ nodejs.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+
+wget -qO- https://deb.nodesource.com/setup_10.x | sudo -E bash -
+sudo apt-get install -y nodejs
---
+++ python3.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+
+sudo apt-get install python3 python3-pip
+python3 -m pip install setuptools
