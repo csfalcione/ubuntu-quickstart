b852ce1206a77130e819f940fe3e4018759c0a7d
Caleb <caleb.falcione@gmail.com>
2019-07-14 18:43:18 -0700

    changes git credential helper to cache

--- git-config.sh
+++ git-config.sh
@@ -8,4 +8,5 @@ read -p "What is your email?: " USER_EMAIL
 git config --global user.name $USER_NAME
 git config --global user.email $USER_EMAIL
 git config --global push.default simple
-git config --global credential.helper store
+# Caches git credentials for 19 hours.
+git config --global credential.helper 'cache --timeout 68400'
