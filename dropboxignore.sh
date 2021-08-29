f1204b2f80cc2e7c126b2dc8122da77a30a7445d
Caleb Falcione <caleb.falcione@gmail.com>
2021-08-29 15:09:31 -0700

    adds script for setting up dropboxignore

---
+++ dropboxignore.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+# Install https://github.com/sp1thas/dropboxignore
+
+sudo apt-get install -y git attr
+
+sudo sh -c "$(curl -fsSL https://raw.githubusercontent.com/sp1thas/dropboxignore/master/utils/install.sh)"
+
