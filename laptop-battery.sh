f9cd39168501110da5454539a3efc33cf3fd9821
Caleb Falcione <caleb.falcione@gmail.com>
2022-07-03 15:41:20 -0400

    update laptop-battery.sh to use updated tlp conf structure

--- laptop-battery.sh
+++ laptop-battery.sh
@@ -3,6 +3,6 @@
 sudo apt-get install -y tlp || (echo Unable to install tlp; exit $?)
 
 # This merely persists wifi and bluetooth device state between restarts.
-sudo sed -i 's/^RESTORE_DEVICE_STATE_ON_STARTUP=0/RESTORE_DEVICE_STATE_ON_STARTUP=1/g' /etc/default/tlp
+echo "RESTORE_DEVICE_STATE_ON_STARTUP=1" | sudo tee /etc/tlp.d/90-persistbluetooth.conf
 
 sudo tlp start
